ce2d854c133a0848b1e4542ca3515398179cd294
ligch <ligch@shanghaitech.edu.cn>
2023-10-26 21:25:59 +0800

    fix update text

--- src/observer/sql/stmt/update_stmt.cpp
+++ src/observer/sql/stmt/update_stmt.cpp
@@ -25,8 +25,13 @@ See the Mulan PSL v2 for more details. */
 #include <cstdint>
 #include <vector>
 
-UpdateStmt::UpdateStmt(Table *table, FilterStmt *filter_stmt, std::vector<const FieldMeta *> &&field_metas, std::unordered_map<size_t, Value> &&value_map, std::unordered_map<size_t, SelectStmt> &&select_map)
-    : table_(table), filter_stmt_(filter_stmt), field_metas_(std::move(field_metas)), value_map_(std::move(value_map)), select_map_(std::move(select_map))
+UpdateStmt::UpdateStmt(Table *table, FilterStmt *filter_stmt, std::vector<const FieldMeta *> &&field_metas,
+    std::unordered_map<size_t, Value> &&value_map, std::unordered_map<size_t, SelectStmt> &&select_map)
+    : table_(table),
+      filter_stmt_(filter_stmt),
+      field_metas_(std::move(field_metas)),
+      value_map_(std::move(value_map)),
+      select_map_(std::move(select_map))
 {}
 
 UpdateStmt::~UpdateStmt()
@@ -40,7 +45,7 @@ UpdateStmt::~UpdateStmt()
 RC UpdateStmt::create(Db *db, const UpdateSqlNode &update, Stmt *&stmt)
 {
   const char *table_name = update.relation_name.c_str();
-  if (nullptr == db || nullptr == table_name ) {
+  if (nullptr == db || nullptr == table_name) {
     LOG_WARN("invalid argument. db=%p, table_name=%p",
         db, table_name);
     return RC::INVALID_ARGUMENT;
@@ -56,23 +61,24 @@ RC UpdateStmt::create(Db *db, const UpdateSqlNode &update, Stmt *&stmt)
   table_map.insert(std::pair<std::string, Table *>(std::string(table_name), table));
 
   FilterStmt *filter_stmt = nullptr;
-  RC rc = FilterStmt::create(db, table, &table_map, update.conditions.data(), static_cast<int>(update.conditions.size()), filter_stmt);
+  RC          rc          = FilterStmt::create(
+      db, table, &table_map, update.conditions.data(), static_cast<int>(update.conditions.size()), filter_stmt);
   if (rc != RC::SUCCESS) {
     LOG_WARN("failed to create filter statement. rc=%d:%s", rc, strrc(rc));
     return rc;
   }
 
-  size_t col_cnt = update.update_list.size();
-  std::vector<const FieldMeta *> field_metas;
-  std::unordered_map<size_t, Value> value_map;
+  size_t                                 col_cnt = update.update_list.size();
+  std::vector<const FieldMeta *>         field_metas;
+  std::unordered_map<size_t, Value>      value_map;
   std::unordered_map<size_t, SelectStmt> select_map;
 
   for (int i = 0; i < col_cnt; ++i) {
-    const auto &update_pair = update.update_list[i];
-    const FieldMeta *field_meta;
+    const auto       &update_pair = update.update_list[i];
+    const FieldMeta  *field_meta;
     const std::string attribute_name = update_pair.attribute_name;
-    Value value = update_pair.value;
-    RC rc = table->get_field_meta_by_name(field_meta, attribute_name);
+    Value             value          = update_pair.value;
+    RC                rc             = table->get_field_meta_by_name(field_meta, attribute_name);
     if (rc != RC::SUCCESS) {
       LOG_WARN("failed to create filter statement. rc=%d:%s", rc, strrc(rc));
       return rc;
@@ -87,30 +93,32 @@ RC UpdateStmt::create(Db *db, const UpdateSqlNode &update, Stmt *&stmt)
               table_name, field_meta->name(), value.get_string().c_str());
           return RC::INVALID_ARGUMENT;
         }
+      } else if (field_meta->type() == TEXTS && value.attr_type() == CHARS) {
+        // continue;
       } else if (field_meta->nullable() && value.is_null()) {
         LOG_DEBUG("field convert null to nullable. table=%s, field=%s, field type=%d, value_type=%d",
           table_name, field_meta->name(), field_meta->type(), value.attr_type());
-      } else if ((rc = value.convert_to(field_meta->type())) != RC::SUCCESS){
+      } else if ((rc = value.convert_to(field_meta->type())) != RC::SUCCESS) {
         rc = RC::SCHEMA_FIELD_TYPE_MISMATCH;
         LOG_WARN("failed to create update statement. rc=%d:%s", rc, strrc(rc));
-        return rc;         
+        return rc;
       }
     }
     field_metas.emplace_back(field_meta);
 
     if (update_pair.is_select) {
       Stmt *select_stmt = nullptr;
-      rc = SelectStmt::create(db, update_pair.select, select_stmt);
+      rc                = SelectStmt::create(db, update_pair.select, select_stmt);
       if (rc != RC::SUCCESS) {
         LOG_ERROR("failed to create select stmt for update. rc=%d:%s", rc, strrc(rc));
         return rc;
       }
-      if (static_cast<SelectStmt*>(select_stmt)->query_exprs().size() != 1) {
+      if (static_cast<SelectStmt *>(select_stmt)->query_exprs().size() != 1) {
         rc = RC::SCHEMA_FIELD_TYPE_MISMATCH;
         LOG_WARN("failed to create update statement, not 1 expr", rc, strrc(rc));
         return rc;
       }
-      select_map.emplace(i, *static_cast<SelectStmt*>(select_stmt));
+      select_map.emplace(i, *static_cast<SelectStmt *>(select_stmt));
     } else {
       value_map.emplace(i, value);
     }
